4d17c16d4c7e08007aacbe9e5250ac1a5165c41e
Koichi Tanaka <k.tanaka@everyleaf.com>
2013-07-25 16:59:27 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManyoDominionNoKai::Application.config.secret_key_base = '6d6876eb80cf2a43014cd2231c2614e4e512caff5b72953e140328ea2adeaf7b3f79bba87939f7b41aea32c6261c2326a01f8c67d852518d06273244a44d667e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ManyoDominionNoKai::Application.config.session_store :cookie_store, key: '_manyo_dominion_no_kai_session'
